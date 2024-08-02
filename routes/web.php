895da2c863efa7c7173ce6ee63e2f226af763b81
nkfas <nkfasnk@gmail.com>
2024-08-02 07:37:22 +0300

    login

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login',[LoginController::class,'login']);
 
 Route::get('admin',[AdminController::class,'index']);
